dc77b29810c99ea70df7c4ae21e70afb5bd2cfe6
Erik <erik.ra@gmail.com>
2017-04-05 23:17:10 +0200

    Add project files.

---
+++ TakeTwoStones/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TakeTwoStones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (int.Parse(Console.ReadLine()) % 2 == 0)
+            {
+                Console.WriteLine("Bob");
+            }
+            else
+            {
+                Console.WriteLine("Alice");
+            }
+        }
+    }
+}
